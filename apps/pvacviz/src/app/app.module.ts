0122afb935250a5845b97ec8708a7b9c6824dd59
Joshua McMichael <josh@jmcmichael.com>
2018-03-08 12:43:19 -0600

    remove uneccessary app events

--- apps/pvacviz/src/app/app.module.ts
+++ apps/pvacviz/src/app/app.module.ts
@@ -7,14 +7,25 @@ import { StoreModule } from "@ngrx/store";
 import { reducers, metaReducers } from "./reducers";
 import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 import { environment } from "../environments/environment";
+import { EffectsModule } from "@ngrx/effects";
 
 @NgModule({
   imports: [
     BrowserModule,
     NxModule.forRoot(),
     RouterModule.forRoot([], { initialNavigation: "enabled" }),
+
+    /**
+     * StoreModule.forRoot is imported once in the root module, accepting a reducer
+     * function or object map of reducer functions. If passed an object of
+     * reducers, combineReducers will be run creating your application
+     * meta-reducer. This returns all providers for an @ngrx/store
+     * based application.
+     */
     StoreModule.forRoot(reducers, { metaReducers }),
+
     !environment.production ? StoreDevtoolsModule.instrument() : [],
+    EffectsModule.forRoot([]),
   ],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
