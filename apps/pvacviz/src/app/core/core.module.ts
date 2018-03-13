cbad29b9bd61c7c70d5817115eb5d491ffa50d77
Joshua McMichael <josh@jmcmichael.com>
2018-03-13 13:29:44 -0500

    moved bootstrap app component to core/containers

--- apps/pvacviz/src/app/core/core.module.ts
+++ apps/pvacviz/src/app/core/core.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AppComponent } from './containers/app/app.component';
+
+
+export const COMPONENTS = [
+  AppComponent,
+];
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: COMPONENTS,
+  exports: COMPONENTS
 })
-export class CoreModule { }
+
+export class CoreModule {
+  static forRoot() {
+    return {
+      ngModule: CoreModule,
+      providers: [],
+    };
+  }
+}
