f23f86927ab3cc30d9636f9f9ca5ab23924f0138
Joshua McMichael <josh@jmcmichael.com>
2018-03-14 16:30:29 -0500

    sidebar show/hide working

--- apps/pvacviz/src/app/core/containers/app/app.component.ts
+++ apps/pvacviz/src/app/core/containers/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'pvz-app',
@@ -6,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  collapsible: boolean = true;
+  collapsed: boolean = true;
 
-  constructor() { }
+  constructor(private router: Router) {
+  }
 
   ngOnInit() {
   }
