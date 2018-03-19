6cf8acdde92d58e30c5983ce6e916ee07f8e56ad
Joshua McMichael <josh@jmcmichael.com>
2018-03-19 14:40:27 -0500

    created processes module

---
+++ apps/pvacviz/src/app/processes/processes.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProcessesRoutingModule } from './processes-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ProcessesRoutingModule
+  ],
+  declarations: []
+})
+export class ProcessesModule { }
