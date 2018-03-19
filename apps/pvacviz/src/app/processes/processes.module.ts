1ce3a9b0393d72ff413005043a022693c3d12151
Joshua McMichael <josh@jmcmichael.com>
2018-03-19 14:45:55 -0500

    added processes module and processes routing module

--- apps/pvacviz/src/app/processes/processes.module.ts
+++ apps/pvacviz/src/app/processes/processes.module.ts
@@ -2,11 +2,31 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProcessesRoutingModule } from './processes-routing.module';
+import { StoreModule } from '@ngrx/store';
+import * as fromProcesses from './reducers/processes.reducer';
+import { EffectsModule } from '@ngrx/effects';
+import { ProcessesEffects } from './effects/processes.effects';
 
 @NgModule({
   imports: [
     CommonModule,
-    ProcessesRoutingModule
+    ProcessesRoutingModule,
+    /**
+     * StoreModule.forFeature is used for composing state
+     * from feature modules. These modules can be loaded
+     * eagerly or lazily and will be dynamically added to
+     * the existing state.
+     */
+    StoreModule.forFeature('processes', fromProcesses.reducer),
+    /**
+     * Effects.forFeature is used to register effects
+     * from feature modules. Effects can be loaded
+     * eagerly or lazily and will be started immediately.
+     *
+     * All Effects will only be instantiated once regardless of
+     * whether they are registered once or multiple times.
+     */
+    EffectsModule.forFeature([ProcessesEffects])
   ],
   declarations: []
 })
