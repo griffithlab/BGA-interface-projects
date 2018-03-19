0f9f015a56c9dcecde464fb002acb124764b2c22
Joshua McMichael <josh@jmcmichael.com>
2018-03-19 13:26:48 -0500

    added routerstateserializer and router utility functions

--- apps/pvacviz/src/app/app.module.ts
+++ apps/pvacviz/src/app/app.module.ts
@@ -1,14 +1,25 @@
+// @angular
 import { NgModule } from "@angular/core";
-import { AppComponent } from "./core/containers/app/app.component";
 import { BrowserModule } from "@angular/platform-browser";
-import { NxModule } from "@nrwl/nx";
 import { RouterModule } from "@angular/router";
+
+// @ngrx
 import { StoreModule } from "@ngrx/store";
-import { reducers, metaReducers } from "./reducers";
-import { StoreDevtoolsModule } from "@ngrx/store-devtools";
-import { environment } from "../environments/environment";
 import { EffectsModule } from "@ngrx/effects";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import {
+  StoreRouterConnectingModule,
+  RouterStateSerializer,
+} from '@ngrx/router-store';
+
+import { NxModule } from "@nrwl/nx";
+
+// pvacviz
+import { CustomRouterStateSerializer } from './shared/utilities';
+import { environment } from "../environments/environment";
+import { reducers, metaReducers } from "./reducers";
 import { CoreModule } from './core/core.module';
+import { AppComponent } from "./core/containers/app/app.component";
 
 @NgModule({
   imports: [
@@ -43,6 +54,14 @@ import { CoreModule } from './core/core.module';
     !environment.production ? StoreDevtoolsModule.instrument() : [],
     EffectsModule.forRoot([]),
     CoreModule.forRoot()
+  ],
+  providers: [
+    /**
+     * The `RouterStateSnapshot` provided by the `Router` is a large complex structure.
+     * A custom RouterStateSerializer is used to parse the `RouterStateSnapshot` provided
+     * by `@ngrx/router-store` to include only the desired pieces of the snapshot.
+     */
+    { provide: RouterStateSerializer, useClass: CustomRouterStateSerializer },
   ],
   bootstrap: [AppComponent],
 })
