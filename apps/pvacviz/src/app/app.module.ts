5ecf3422821027de1c8c530e03b2d03ee3b49e81
Joshua McMichael <josh@jmcmichael.com>
2018-03-19 13:08:29 -0500

    updated nrwl, schematics

--- apps/pvacviz/src/app/app.module.ts
+++ apps/pvacviz/src/app/app.module.ts
@@ -17,6 +17,21 @@ import { CoreModule } from './core/core.module';
     RouterModule.forRoot([], { initialNavigation: "enabled" }),
 
     /**
+     * Store devtools instrument the store retaining past versions of state
+     * and recalculating new states. This enables powerful time-travel
+     * debugging.
+     *
+     * To use the debugger, install the Redux Devtools extension for either
+     * Chrome or Firefox
+     *
+     * See: https://github.com/zalmoxisus/redux-devtools-extension
+     */
+    StoreDevtoolsModule.instrument({
+      name: 'pVACviz Store DevTools',
+      logOnly: environment.production,
+    }),
+
+    /**
      * StoreModule.forRoot is imported once in the root module, accepting a reducer
      * function or object map of reducer functions. If passed an object of
      * reducers, combineReducers will be run creating your application
