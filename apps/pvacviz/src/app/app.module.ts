0f212fef1aa6be6e4f274d806f9909dc429c64f0
Joshua McMichael <josh@jmcmichael.com>
2018-03-20 10:21:35 -0500

    router state now reflected in Store

--- apps/pvacviz/src/app/app.module.ts
+++ apps/pvacviz/src/app/app.module.ts
@@ -30,6 +30,26 @@ import { routes } from './routes';
     RouterModule.forRoot(routes, { initialNavigation: "enabled" }),
 
     /**
+     * StoreModule.forRoot is imported once in the root module, accepting a reducer
+     * function or object map of reducer functions. If passed an object of
+     * reducers, combineReducers will be run creating your application
+     * meta-reducer. This returns all providers for an @ngrx/store
+     * based application.
+     */
+    StoreModule.forRoot(reducers, { metaReducers }),
+
+    /**
+     * @ngrx/router-store keeps router state up-to-date in the store.
+     */
+    StoreRouterConnectingModule.forRoot({
+      /*
+        They stateKey defines the name of the state used by the router-store reducer.
+        This matches the key defined in the map of reducers
+      */
+      stateKey: 'router',
+    }),
+
+    /**
      * Store devtools instrument the store retaining past versions of state
      * and recalculating new states. This enables powerful time-travel
      * debugging.
@@ -39,21 +59,18 @@ import { routes } from './routes';
      *
      * See: https://github.com/zalmoxisus/redux-devtools-extension
      */
-    StoreDevtoolsModule.instrument({
+    !environment.production ? StoreDevtoolsModule.instrument({
       name: 'pVACviz Store DevTools',
       logOnly: environment.production,
-    }),
+    }) : [],
 
     /**
-     * StoreModule.forRoot is imported once in the root module, accepting a reducer
-     * function or object map of reducer functions. If passed an object of
-     * reducers, combineReducers will be run creating your application
-     * meta-reducer. This returns all providers for an @ngrx/store
-     * based application.
+     * EffectsModule.forRoot() is imported once in the root module and
+     * sets up the effects class to be initialized immediately when the
+     * application starts.
+     *
+     * See: https://github.com/ngrx/platform/blob/master/docs/effects/api.md#forroot
      */
-    StoreModule.forRoot(reducers, { metaReducers }),
-
-    !environment.production ? StoreDevtoolsModule.instrument() : [],
     EffectsModule.forRoot([]),
     CoreModule.forRoot()
   ],
