b13280943193a07a0669f2562bad8baa0534d7eb
Joshua McMichael <josh@jmcmichael.com>
2018-03-26 12:25:05 -0500

    added start to routes, updated sidebare menu

--- apps/pvacviz/src/app/manage/manage-routing.module.ts
+++ apps/pvacviz/src/app/manage/manage-routing.module.ts
@@ -2,12 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ManagePageComponent } from './containers/manage-page/manage-page.component';
 const routes: Routes = [
-  // { path: 'find', component: FindBookPageComponent },
-  // {
-  //   path: ':id',
-  //   component: ViewBookPageComponent,
-  //   canActivate: [BookExistsGuard],
-  // },
   { path: '', component: ManagePageComponent },
 ];
 
