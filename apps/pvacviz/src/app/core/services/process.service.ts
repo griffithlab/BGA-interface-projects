1e1f9c48cba1ec85cd250113219d6df689f2f406
Joshua McMichael <josh@jmcmichael.com>
2018-03-26 16:56:25 -0500

    starting work on form

--- apps/pvacviz/src/app/core/services/process.service.ts
+++ apps/pvacviz/src/app/core/services/process.service.ts
@@ -1,22 +1,40 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { map } from 'rxjs/operators';
 import { Process } from '../../manage/models/process.model';
 
 @Injectable()
 export class ProcessService {
-  private API_PATH = 'http://localhost:8080/api/v1/processes';
+  private API_PATH = 'http://localhost:8080/api/v1';
 
   constructor(private http: HttpClient) { }
 
   query(): Observable<Process[]> {
     return this.http
-      .get<{ result: Process[] }>(`${this.API_PATH}`)
+      .get<{ result: Process[] }>(`${this.API_PATH}/processes`)
       .pipe(map(processes => processes.result || []));
   }
 
-  // retrieveBook(volumeId: string): Observable<Book> {
-  //   return this.http.get<Book>(`${this.API_PATH}/${volumeId}`);
+  start(processParameters: any): Observable<any> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    }
+    const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded' });
+
+    return this.http.post<number>(`${this.API_PATH}/staging`, processParameters, httpOptions)
+      .pipe((response: Response) => {
+        console.log(response);
+        return JSON.parse(response['_body']) as number;
+      });
+  }
+
+  // archive(id: number): Observable<string> {
+  //   return this.http.get(`${this.api}/archive/${id}`)
+  //     .map((response: Response) => {
+  //       return response.statusText;
+  //     });
   // }
 }
--- apps/pvacviz/src/app/start/containers/start-page/start-page.component.ts
+++ apps/pvacviz/src/app/start/containers/start-page/start-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
   selector: 'pvz-start-page',
