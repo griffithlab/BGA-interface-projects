faf805250c3bb9e9b08fd8f2387a27b41e7458a8
Joshua McMichael <josh@jmcmichael.com>
2018-03-26 11:57:25 -0500

    created start view folder, module, route files

---
+++ apps/pvacviz/src/app/start/start-routing.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { StartPageComponent } from './containers/start-page/start-page.component';
+const routes: Routes = [
+  { path: '', component: StartPageComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class StartRoutingModule { }
---
+++ apps/pvacviz/src/app/start/start.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ClarityModule } from '@clr/angular';
+import { StartRoutingModule } from './start-routing.module';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StartPageComponent } from './containers/start-page/start-page.component';
+
+import { reducers } from './reducers';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ClarityModule,
+    StartRoutingModule,
+    /**
+     * StoreModule.forFeature is used for composing state
+     * from feature modules. These modules can be loaded
+     * eagerly or lazily and will be dynamically added to
+     * the existing state.
+     */
+    StoreModule.forFeature('processes', reducers),
+
+    /**
+     * Effects.forFeature is used to register effects
+     * from feature modules. Effects can be loaded
+     * eagerly or lazily and will be started immediately.
+     *
+     * All Effects will only be instantiated once regardless of
+     * whether they are registered once or multiple times.
+     */
+    EffectsModule.forFeature([ProcessEffects])
+  ],
+  declarations: [StartPageComponent]
+})
+export class StartModule { }
