e23ec7358fddfe2f791121f0f048d67cc52f0166
Joshua McMichael <josh@jmcmichael.com>
2018-03-26 13:36:11 -0500

    added visualize view, module, routes

---
+++ apps/pvacviz/src/app/visualize/containers/visualize-page/visualize-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pvz-visualize-page',
+  templateUrl: './visualize-page.component.html',
+  styleUrls: ['./visualize-page.component.scss']
+})
+export class VisualizePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
