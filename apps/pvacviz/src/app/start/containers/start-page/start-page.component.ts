f8df2f44b2a2af7825f7059cf71f62513b000045
Joshua McMichael <josh@jmcmichael.com>
2018-03-27 15:19:23 -0500

    implemented inputs call on start-page

--- apps/pvacviz/src/app/start/containers/start-page/start-page.component.ts
+++ apps/pvacviz/src/app/start/containers/start-page/start-page.component.ts
@@ -6,14 +6,30 @@ import {
   Validators
 } from "@angular/forms";
 
+import { Observable } from 'rxjs/Rx';
+
+import { Store, select } from '@ngrx/store';
+
+import { File, Files } from '../../../core/models/file.model';
+import { InputService } from '../../../core/services/inputs.service';
+
+import * as fromStartActions from '../../actions/start.actions';
+import * as fromStart from '../../reducers';
+
 @Component({
   selector: 'pvz-start-page',
   templateUrl: './start-page.component.html',
   styleUrls: ['./start-page.component.scss']
 })
-export class StartPageComponent implements OnInit {
 
-  constructor() { }
+export class StartPageComponent implements OnInit {
+  // inputs$: Observable<Files>;
+  inputs$: Observable<any>;
+
+  constructor(private store: Store<fromStart.State>) {
+    this.inputs$ = store.pipe(select(fromStart.getAllInputs))
+      .map((fileMap) => fileMap);
+  }
 
   ngOnInit() {
   }
