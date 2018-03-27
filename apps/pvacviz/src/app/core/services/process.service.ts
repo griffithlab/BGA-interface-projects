a35f66836d7b8d9ac094c52653d522dcff00b07a
Joshua McMichael <josh@jmcmichael.com>
2018-03-27 10:00:25 -0500

    temporarily uncommented problematic function to do some housecleaning

--- apps/pvacviz/src/app/core/services/process.service.ts
+++ apps/pvacviz/src/app/core/services/process.service.ts
@@ -16,20 +16,20 @@ export class ProcessService {
       .pipe(map(processes => processes.result || []));
   }
 
-  start(processParameters: any): Observable<any> {
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json'
-      })
-    }
-    const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded' });
+  // start(processParameters: any): Observable<any> {
+  //   const httpOptions = {
+  //     headers: new HttpHeaders({
+  //       'Content-Type': 'application/json'
+  //     })
+  //   }
+  //   const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded' });
 
-    return this.http.post<number>(`${this.API_PATH}/staging`, processParameters, httpOptions)
-      .pipe((response: Response) => {
-        console.log(response);
-        return JSON.parse(response['_body']) as number;
-      });
-  }
+  //   return this.http.post<number>(`${this.API_PATH}/staging`, processParameters, httpOptions)
+  //     .pipe((response: Response) => {
+  //       console.log(response);
+  //       return JSON.parse(response['_body']) as number;
+  //     });
+  // }
 
   // archive(id: number): Observable<string> {
   //   return this.http.get(`${this.api}/archive/${id}`)
