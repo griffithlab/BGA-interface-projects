236aa489c7f93a6b7742a90e4343830f7bf83e04
Joshua McMichael <josh@jmcmichael.com>
2018-05-07 17:14:32 -0500

    added process observers and OnInit in process-page component

--- apps/pvacviz/src/app/manage/containers/process-page/process-page.component.ts
+++ apps/pvacviz/src/app/manage/containers/process-page/process-page.component.ts
@@ -1,16 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Store, select } from '@ngrx/store';
+
+import { Process } from '../../../core/models/process.model';
+import { Observable } from 'rxjs/Observable';
+
+import * as processes from '../../actions/manage.actions';
+import * as fromProcesses from '../../reducers';
 
 @Component({
   selector: 'pvz-process-page',
   templateUrl: './process-page.component.html',
-  styleUrls: ['./process-page.component.scss']
+  styleUrls: ['./process-page.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 
 export class ProcessPageComponent implements OnInit {
 
-  constructor() { }
+  process$: Observable<Process>;
+
+  constructor(private store: Store<fromProcesses.State>) {
+    // TODO: integrate router and get id from state params
+    this.process$ = store.pipe(select(fromProcesses.getProcess(processId)));
+  }
 
   ngOnInit() {
+    this.store.dispatch(new processes.LoadDetail(processId));
   }
 
 }
