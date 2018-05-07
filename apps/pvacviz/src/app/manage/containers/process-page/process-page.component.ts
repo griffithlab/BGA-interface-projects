681b32b8ad06f8534133494cf6ad480c31a69e4a
Joshua McMichael <josh@jmcmichael.com>
2018-05-07 15:27:00 -0500

    added process detail page

---
+++ apps/pvacviz/src/app/manage/containers/process-page/process-page.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pvz-process-page',
+  templateUrl: './process-page.component.html',
+  styleUrls: ['./process-page.component.scss']
+})
+
+export class ProcessPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
