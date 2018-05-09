ddd01ff19d1cea5a22cddcc6bb5c54c0c529681a
Joshua McMichael <josh@jmcmichael.com>
2018-05-09 08:27:57 -0500

    moved RouterStateUrl to its own model file

--- apps/pvacviz/src/app/manage/reducers/index.ts
+++ apps/pvacviz/src/app/manage/reducers/index.ts
@@ -31,6 +31,7 @@ export const reducers = {
  * ```
  */
 
+export const getRouterState = createFeatureSelector<fromNgrxRouter.RouterReducerState<RouterStateUrl>>('router');
 /**
  * The createFeatureSelector function selects a piece of state from the root of the state object.
  * This is used for selecting feature states that are loaded eagerly or lazily.
