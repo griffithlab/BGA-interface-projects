dc5126965b81d4faab0d0de6ce7be81081c248b6
Joshua McMichael <josh@jmcmichael.com>
2018-05-21 11:20:14 -0500

    added processes selectors to vis page - won't work w/o refactoring processes to core

--- apps/pvacviz/src/app/visualize/containers/visualize-page/visualize-page.component.ts
+++ apps/pvacviz/src/app/visualize/containers/visualize-page/visualize-page.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Store, select } from '@ngrx/store';
+
+import * as processes from '../../../manage/actions/manage.actions';
+import * as fromProcesses from '../../../manage/reducers';
+
+import { Process } from '../../../core/models/process.model';
+import { Observable } from 'rxjs/Observable';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'pvz-visualize-page',
@@ -6,8 +14,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./visualize-page.component.scss']
 })
 export class VisualizePageComponent implements OnInit {
+  processes$: Observable<Process[]>;
 
-  constructor() { }
+  constructor(private store: Store<fromProcesses.State>) {
+    // this.processes$ = store.pipe(select(fromProcesses.getAllProcesses));
+  }
 
   ngOnInit() {
   }
