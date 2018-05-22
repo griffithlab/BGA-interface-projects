c2d4fb95f6b71efab07d56e193f7172a28f2348c
Joshua McMichael <josh@jmcmichael.com>
2018-05-21 23:01:10 -0500

    created process/file list on visuzlize page

--- apps/pvacviz/src/app/visualize/containers/visualize-page/visualize-page.component.ts
+++ apps/pvacviz/src/app/visualize/containers/visualize-page/visualize-page.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Store, select } from '@ngrx/store';
 
-import * as processes from '../../../manage/actions/manage.actions';
-import * as fromProcesses from '../../../manage/reducers';
-
-import { Process } from '../../../core/models/process.model';
 import { Observable } from 'rxjs/Observable';
 import { filter, map } from 'rxjs/operators';
+
+import { Process } from '../../../core/models/process.model';
+import * as processes from '../../../core/actions/process.actions';
+import * as fromCore from '../../../core/reducers';
 
 @Component({
   selector: 'pvz-visualize-page',
@@ -16,11 +16,15 @@ import { filter, map } from 'rxjs/operators';
 export class VisualizePageComponent implements OnInit {
   processes$: Observable<Process[]>;
 
-  constructor(private store: Store<fromProcesses.State>) {
-    // this.processes$ = store.pipe(select(fromProcesses.getAllProcesses));
+  constructor(private store: Store<fromCore.State>) {
+    this.processes$ = store.pipe(select(fromCore.getAllProcesses));
   }
 
   ngOnInit() {
+    this.store.dispatch(new processes.Load());
   }
 
+  reload() {
+    this.store.dispatch(new processes.Load());
+  }
 }
