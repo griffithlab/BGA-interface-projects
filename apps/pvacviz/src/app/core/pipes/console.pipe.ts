30cc3d5e7b3e9cd0509077e629222e93a97d6c0c
Joshua McMichael <josh@jmcmichael.com>
2018-07-24 09:58:04 -0500

    added console pipe for easier template debugging

---
+++ apps/pvacviz/src/app/core/pipes/console.pipe.ts
@@ -0,0 +1,11 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'console'
+})
+export class ConsolePipe implements PipeTransform {
+  transform(value: any): string {
+    console.log(value);
+    return '';
+  }
+}
