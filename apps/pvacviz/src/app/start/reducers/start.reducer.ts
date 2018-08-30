23e6e4057f33cadc90c3efaed6fc323e1f605a5c
Joshua McMichael <josh@jmcmichael.com>
2018-08-30 11:43:22 -0500

    validation of boxed array form controls now working properly

--- apps/pvacviz/src/app/start/reducers/start.reducer.ts
+++ apps/pvacviz/src/app/start/reducers/start.reducer.ts
@@ -2,19 +2,106 @@ import { createSelector, combineReducers, Action } from '@ngrx/store';
 import { createEntityAdapter, EntityAdapter, EntityState } from '@ngrx/entity';
 import {
   createFormGroupState,
+  createFormStateReducerWithUpdate,
   formGroupReducer,
   FormGroupState,
+  Boxed,
+  box,
+  unbox,
+  validate,
+  updateGroup,
+  compose,
+  updateArray
 } from 'ngrx-forms';
-import { required } from 'ngrx-forms/validation';
+import { required, minLength } from 'ngrx-forms/validation';
 
 import { File, Files } from '@pvz/core/models/file.model';
-import {
-  StartFormGroupValue,
-  StartFormGroupInitialState,
-  updateStartFormGroup
-} from '@pvz/start/models/start-form.models';
+import { Allele } from '@pvz/core/models/api-responses.model';
 import { StartActions, StartActionTypes, StartProcessSuccess, SetSubmittedValueAction } from '@pvz/start/actions/start.actions';
 import { ApiStartResponse } from '@pvz/core/models/api-responses.model';
+
+export interface StartFormGroupValue {
+  'input': string;
+  'samplename': string;
+  'alleles': Boxed<string[]>;
+  'prediction_algorithms': Boxed<string[]>;
+  'epitope_lengths': Boxed<string[]>;
+  'peptide_sequence_length': number;
+  'net_chop_method': string;
+  'net_chop_threshold': number;
+  'netmhc_stab': boolean;
+  'top_score_metric': string;
+  'binding_threshold': number;
+  'allele_specific_cutoffs': boolean;
+  'minimum_fold_change': number;
+  'expn_val': number;
+  'normal_cov': number;
+  'tdna_cov': number;
+  'trna_cov': number;
+  'normal_vaf': number;
+  'tdna_vaf': number;
+  'trna_vaf': number;
+  'fasta_size': number;
+  'iedb_retries': number;
+  'downstream_sequence_length': number;
+  'iedb_install_dir': string;
+  'keep_tmp_files': boolean;
+  'force': boolean;
+}
+export const StartFormGroupInitialState = {
+  'input': '',
+  'samplename': '',
+  'alleles': box([]),
+  'prediction_algorithms': box([]),
+  'epitope_lengths': box(['10']),
+  'peptide_sequence_length': 21,
+  'net_chop_method': '',
+  'net_chop_threshold': 0.5,
+  'netmhc_stab': false,
+  'top_score_metric': 'median',
+  'binding_threshold': 500,
+  'allele_specific_cutoffs': false,
+  'minimum_fold_change': 0,
+  'expn_val': 1,
+  'normal_cov': 5,
+  'tdna_cov': 5,
+  'trna_cov': 5,
+  'normal_vaf': 5,
+  'tdna_vaf': 5,
+  'trna_vaf': 5,
+  'fasta_size': 200,
+  'iedb_retries': 5,
+  'downstream_sequence_length': 1000,
+  'iedb_install_dir': '',
+  'keep_tmp_files': false,
+  'force': false,
+}
+
+// validation
+// TODO keep checking for v3.1.0, which adds transparent validation for boxed values, eliminating need for all the unbox() crap below:
+// https://github.com/MrWolfZ/ngrx-forms/issues/96
+export const updateStartFormGroup = createFormStateReducerWithUpdate<StartFormGroupValue>(updateGroup<StartFormGroupValue>({
+  input: validate(required),
+  samplename: validate(required, minLength(2)),
+  prediction_algorithms: validate(value => required(unbox(value)), value => minLength(1)(unbox(value))),
+  alleles: validate(value => required(unbox(value)), value => minLength(1)(unbox(value))),
+  epitope_lengths: validate(value => required(unbox(value)), value => minLength(1)(unbox(value))),
+  peptide_sequence_length: validate(required),
+  net_chop_threshold: validate(required),
+  top_score_metric: validate(required),
+  binding_threshold: validate(required),
+  minimum_fold_change: validate(required),
+  expn_val: validate(required),
+  normal_cov: validate(required),
+  tdna_cov: validate(required),
+  trna_cov: validate(required),
+  normal_vaf: validate(required),
+  tdna_vaf: validate(required),
+  trna_vaf: validate(required),
+  fasta_size: validate(required),
+  iedb_retries: validate(required),
+  downstream_sequence_length: validate(required),
+}));
 
 /**
  * FORM STATE AND REDUCER
@@ -31,7 +118,7 @@ export const INITIAL_STATE = createFormGroupState<StartFormGroupValue>(FORM_ID, 
 
 const formReducers = combineReducers<FormState, any>({
   state(s = INITIAL_STATE, a: Action) {
-    return updateStartFormGroup(formGroupReducer(s, a));
+    return updateStartFormGroup(s, a);
   },
   submittedValue(s: StartFormGroupValue | undefined, a: SetSubmittedValueAction) {
     switch (a.type) {
