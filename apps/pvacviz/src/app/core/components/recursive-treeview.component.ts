bbb9bc0c94ef611b37b05a46ce6074bced5332c5
Joshua McMichael <josh@jmcmichael.com>
2018-09-25 08:13:17 -0500

    added treeview component

---
+++ apps/pvacviz/src/app/core/components/recursive-treeview.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: "recursive-treeview",
+  templateUrl: './recursive-treeview.component.html'
+})
+export class RecursiveTreeviewComponent {
+  @Input() item: any;
+  @Input() selected: boolean = false;
+}
